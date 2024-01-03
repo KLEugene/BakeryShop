2def2cc54b9d75644fd617eebb65e64923f27d4b
KLEugene <kaeugene95@gmail.com>
2024-01-03 19:05:55 +0000

    fix mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,18 @@
 const btnOpen=document.querySelector('.header__open-menu');
 const btnClose=document.querySelector('.mobile-menu__button--close');
 const mobileMenu=document.querySelector('.mobile-menu');
-const menuLink=document.querySelectorAll('.mobile-menu__link');
+const menuLink=document.querySelector('.mobile-menu__list');
 
-console.log(menuLink)
+
 const handleClick=()=>{
     mobileMenu.classList.toggle('is-open')
 }
 btnOpen.addEventListener('click', handleClick);
 btnClose.addEventListener('click', handleClick);
-menuLink.forEach(link=>link.addEventListener('click',handleClick))
+menuLink.addEventListener('click',handleLinkClick)
 
-//console.dir(window)
+function handleLinkClick(event){
+const activeLink=event.target;
+activeLink.classList.toggle('active')
+mobileMenu.classList.toggle('is-open')
+};
