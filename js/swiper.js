311113f791c5ba0d1193789e410fbc8e5f4d1b0a
KLEugene <kaeugene95@gmail.com>
2024-01-23 20:09:35 +0000

    fix cards

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,31 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
 
-  swiper.init()
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 1280px
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+  navigation: {
+    nextEl: ".carusel-button.next",
+    prevEl: ".carusel-button.prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    bulletClass: ".pagination__button",
+    bulletActiveClass: ".pagination__button--active",
+    clickable: true,
+  },
+});
+
+swiper.init();
