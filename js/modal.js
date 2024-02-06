c256ee29392eeb1541e9f495773b824f52872671
KLEugene <kaeugene95@gmail.com>
2024-02-06 19:55:48 +0000

    fixed backgrouds, and fixed modal feedback form

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,12 @@
 const headerButton=document.querySelector('.header__button');
 const modalClose=document.querySelector('[data-modal-close]');
-const modal=document.querySelector('.modal')
-headerButton.addEventListener('click', handleModalClick);
-modalClose.addEventListener('click', handleModalClick);
+const modal = document.querySelector('.modal')
 
+console.dir(modalClose)
+console.log(modal)
+headerButton.addEventListener('click', handleModalClick);
+//modalClose.map((button) => button.addEventListener("click", handleModalClick));
+modalClose.addEventListener("click", handleModalClick)
 function handleModalClick(event){
     modal.classList.toggle('is-hidden');
 }
